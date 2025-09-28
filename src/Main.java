8a5b61ec62b1eae2fbbfa470f9fe114dbb6517b5
olekasba <olekasba@mail.uc.edu>
2025-09-28 11:57:35 -0400

    Finalized Task 1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
+        Scanner in = new Scanner(System.in);
+        double initialPrice = 0;
+        double taxRate = 0.02;
+        double totalPrice = 0;
         System.out.print("What is the price of the item?");
+
+        if(in.hasNextDouble())
+        {
+            initialPrice = in.nextDouble();
+            if(initialPrice > 100)
+            {
+                System.out.println("Your shipping is free!");
+            }
+            else
+            {
+                totalPrice = initialPrice * taxRate;}
+                System.out.println("Your shipping will cost $" + totalPrice);
+            }
+        }
 
 
     }
-}
