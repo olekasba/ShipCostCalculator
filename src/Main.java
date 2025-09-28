435913d007d7c45e639a8474caadcf0a1bcb482c
olekasba <olekasba@mail.uc.edu>
2025-09-28 11:44:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the price of the item?");
+
+
+    }
+}
